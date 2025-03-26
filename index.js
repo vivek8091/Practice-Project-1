510547b62802578d3651675072f47bd6aea22f06
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-03-26 11:11:55 +0530

    setup

--- index.js
+++ index.js
@@ -14,164 +14,164 @@ app.get("/", (req, res) => {
   res.send("server is running...");
 });
 
-// app.post("/Datapost", (req, res) => {
-//   // Inserting data using POST...
-//   const data = {
-//     // Storing data in "data" object...
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   };
+app.post("/Datapost", (req, res) => {
+  // Inserting data using POST...
+  const data = {
+    // Storing data in "data" object...
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-//   conn.query("insert into reg set ?", data, (err, result) => {
-//     // Applying insert query...
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       return res.json({
-//         // Returning res(response) in .json format...
-//         success: true,
-//         message: "Data inserted successfully...",
-//         data: result,
-//       });
-//     }
-//   });
-// });
+  conn.query("insert into reg set ?", data, (err, result) => {
+    // Applying insert query...
+    if (err) {
+      console.log(err);
+    } else {
+      return res.json({
+        // Returning res(response) in .json format...
+        success: true,
+        message: "Data inserted successfully...",
+        data: result,
+      });
+    }
+  });
+});
 
-// app.get("/getData", (req, res) => {
-//   // Fetching(getting) all data from the table...
-//   conn.query("select * from reg", (err, result) => {
-//     // Select query...
-//     if (err) {
-//       console.log(err);
-//       // res.status(500).json({ success: false, message: "Database error" });
-//     } else {
-//       // res.json({ success: true, data: result});
-//       res.send(result);
-//     }
-//   });
-// });
+app.get("/getData", (req, res) => {
+  // Fetching(getting) all data from the table...
+  conn.query("select * from reg", (err, result) => {
+    // Select query...
+    if (err) {
+      console.log(err);
+      // res.status(500).json({ success: false, message: "Database error" });
+    } else {
+      // res.json({ success: true, data: result});
+      res.send(result);
+    }
+  });
+});
 
-// app.get("/getData/:id", (req, res) => {
-//   // Fetching data by it's id...
-//   const id = req.params.id;
-//   conn.query("select * from reg where id = ?", id, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.send(result);
-//     }
-//   });
-// });
+app.get("/getData/:id", (req, res) => {
+  // Fetching data by it's id...
+  const id = req.params.id;
+  conn.query("select * from reg where id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
-// app.patch("/updateData/:id", (req, res) => {
-//   // Updating data by it's id...
-//   const userId = req.params.id; // Getting id from URL...
-//   const { name, email, password } = req.body;
+app.patch("/updateData/:id", (req, res) => {
+  // Updating data by it's id...
+  const userId = req.params.id; // Getting id from URL...
+  const { name, email, password } = req.body;
 
-//   const updateQuery =
-//     "update reg set name = ?, email = ?, password = ? where id = ?";
+  const updateQuery =
+    "update reg set name = ?, email = ?, password = ? where id = ?";
 
-//   conn.query(updateQuery, [name, email, password, userId], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//       return res.json({
-//         Message: "Error Occured...",
-//       });
-//     } else {
-//       return res.json({
-//         success: 1,
-//         Message: "Data updated successfully...",
-//       });
-//     }
-//   });
-// });
+  conn.query(updateQuery, [name, email, password, userId], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.json({
+        Message: "Error Occured...",
+      });
+    } else {
+      return res.json({
+        success: 1,
+        Message: "Data updated successfully...",
+      });
+    }
+  });
+});
 
-// app.delete("/deleteData/:id", (req, res) => {
-//   // Delete data by id...
-//   const userId = req.params.id; // Getting id...
+app.delete("/deleteData/:id", (req, res) => {
+  // Delete data by id...
+  const userId = req.params.id; // Getting id...
 
-//   const deleteQuery = "delete from reg where id =  ?"; // Delete query...
-//   conn.query(deleteQuery, [userId], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       return res.json({
-//         success: 1,
-//         Message: "Data deleted successfully...",
-//       });
-//     }
-//   });
-// });
+  const deleteQuery = "delete from reg where id =  ?"; // Delete query...
+  conn.query(deleteQuery, [userId], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.json({
+        success: 1,
+        Message: "Data deleted successfully...",
+      });
+    }
+  });
+});
 
 // Storage Engine...
-// const storage = multer.diskStorage({
-//   destination: "./uploads/", // Folder to store images
-//   filename: (req, file, cb) => {
-//     cb(
-//       null,
-//       file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-//     );
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: "./uploads/", // Folder to store images
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
 // File Upload Middleware
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: (req, file, cb) => {
-//     const fileTypes = /jpeg|jpg|png|gif/;
-//     const extName = fileTypes.test(
-//       path.extname(file.originalname).toLowerCase()
-//     );
-//     const mimeType = fileTypes.test(file.mimetype);
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const extName = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimeType = fileTypes.test(file.mimetype);
 
-//     if (extName && mimeType) {
-//       return cb(null, true);
-//     } else {
-//       return cb(new Error("Only images (JPG, PNG, GIF) are allowed!"));
-//     }
-//   },
-// });
+    if (extName && mimeType) {
+      return cb(null, true);
+    } else {
+      return cb(new Error("Only images (JPG, PNG, GIF) are allowed!"));
+    }
+  },
+});
 
-// app.post("/upload", upload.array("images", 5), (req, res) => {
-//   // Upload multiple images at a time...
-//   if (!req.files || req.files.length === 0) {
-//     return res.status(400).json({ message: "No files uploaded" });
-//   }
+app.post("/upload", upload.array("images", 5), (req, res) => {
+  // Upload multiple images at a time...
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).json({ message: "No files uploaded" });
+  }
 
-//   const imagePaths = req.files.map((file) => file.filename);
-//   const query = "INSERT INTO images (image_path) VALUES ?";
-//   const values = imagePaths.map((image) => [image]);
+  const imagePaths = req.files.map((file) => file.filename);
+  const query = "INSERT INTO images (image_path) VALUES ?";
+  const values = imagePaths.map((image) => [image]);
 
-//   conn.query(query, [values], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//       return res.status(500).json({ message: "Database error" });
-//     }
-//     return res.json({
-//       success: true,
-//       message: "Images uploaded successfully",
-//       files: imagePaths,
-//     });
-//   });
-// });
+  conn.query(query, [values], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+    return res.json({
+      success: true,
+      message: "Images uploaded successfully",
+      files: imagePaths,
+    });
+  });
+});
 
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
-// app.get("/getImg", (req, res) => {
-//   conn.query("select * from images", (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       // res.json({
-//       //   success: true,
-//       //   message: "Images successfully fetched...",
-//       //   data: result,
-//       // });
-//       res.send(result);
-//     }
-//   });
-// });
+app.get("/getImg", (req, res) => {
+  conn.query("select * from images", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // res.json({
+      //   success: true,
+      //   message: "Images successfully fetched...",
+      //   data: result,
+      // });
+      res.send(result);
+    }
+  });
+});
 
 // API to add values with multiple datatypes...
 app.post("/registration", (req, res) => {
@@ -236,7 +236,6 @@ app.post("/changeLastLogin", (req, res) => {
         console.log(err);
       }
       if (results.length === 0) {
-        // If no user found, return error response
         return res
           .status(401)
           .json({ success: false, message: "Invalid email or password" });
